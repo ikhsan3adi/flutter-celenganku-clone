d0e3fdd4f133ecff00274e4f90648937d23c9a17
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-03-18 18:44:53 +0700

    On Going list view + FAB

--- lib/on_going/view/on_going_page.dart
+++ lib/on_going/view/on_going_page.dart
@@ -6,6 +6,20 @@ class OnGoingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const OnGoingScreen();
+    ThemeData theme = Theme.of(context);
+    return Scaffold(
+      body: const OnGoingScreen(),
+      floatingActionButton: ElevatedButton.icon(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: theme.primaryColorLight,
+          foregroundColor: theme.primaryColorDark,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          padding: const EdgeInsets.all(20),
+        ),
+        icon: const Icon(Icons.add),
+        label: const Text("Tambah Celengan", style: TextStyle(fontSize: 18)),
+      ),
+    );
   }
 }
