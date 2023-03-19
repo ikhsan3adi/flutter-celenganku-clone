df2efea81cbfc7d7e8b01d45c85b0510bde25857
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-03-19 13:44:14 +0700

    Update main.dart use hydrated_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:celenganku_app_clone/app.dart';
 import 'package:celenganku_app_clone/my_bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+
   Bloc.observer = MyBlocObserver();
+
   runApp(const MyApp());
 }
