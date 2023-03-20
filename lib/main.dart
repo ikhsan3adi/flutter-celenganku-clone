f744851649123b366fb6f8aa6e7b46925a561755
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-03-20 19:34:22 +0700

    Add ternary for web storage directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:celenganku_app_clone/app.dart';
 import 'package:celenganku_app_clone/my_bloc_observer.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
@@ -8,7 +9,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: await getApplicationDocumentsDirectory(),
+    storageDirectory: kIsWeb ? HydratedStorage.webStorageDirectory : await getApplicationDocumentsDirectory(),
   );
 
   Bloc.observer = MyBlocObserver();
