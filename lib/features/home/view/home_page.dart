96e6a626880b39f34d9673eab88600929cc2225a
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-03-21 19:41:00 +0700

    Update home_page.dart
    Remove multiblocprovider

--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -1,20 +1,11 @@
-import 'package:celenganku_app_clone/features/achieved/bloc/achieved_bloc.dart';
 import 'package:celenganku_app_clone/features/home/home.dart';
-import 'package:celenganku_app_clone/features/on_going/bloc/on_going_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => OnGoingBloc()),
-        BlocProvider(create: (context) => AchievedBloc()),
-      ],
-      child: const HomeScreen(),
-    );
+    return const HomeScreen();
   }
 }
