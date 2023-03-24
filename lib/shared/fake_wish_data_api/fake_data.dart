db65652ec74991d063eb4297c8bf8233a88c636a
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-03-24 10:39:17 +0700

    Update fake_data.dart
    Add saving list

--- lib/shared/fake_wish_data_api/fake_data.dart
+++ lib/shared/fake_wish_data_api/fake_data.dart
@@ -8,7 +8,11 @@ class FakeWishList {
       savingTarget: 2000000,
       savingPlan: SavingPlan.weekly,
       savingNominal: 50000,
-      listSaving: const [],
+      listSaving: [
+        Saving(savingNominal: 15000, createdAt: DateTime(2023, 1, 1, 7, 25), message: 'Awal tahun'),
+        Saving(savingNominal: -5000, createdAt: DateTime(2023, 1, 3, 11, 29), message: 'Pinjam goceng'),
+        Saving(savingNominal: 10000, createdAt: DateTime(2023, 1, 5, 16, 56), message: 'Awal tahun'),
+      ],
       createdAt: DateTime.parse('2023-03-14'),
       completedAt: null,
       imagePath: null,
