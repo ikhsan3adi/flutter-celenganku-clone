8a50525f082b9b8e121bb4300a52b5c372ef28f0
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-03-25 23:07:33 +0700

    Move & delete wish image file
    Move image from cache to app document directory

--- lib/shared/repository/wish_repository.dart
+++ lib/shared/repository/wish_repository.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:celenganku_app_clone/shared/shared.dart';
 import 'package:equatable/equatable.dart';
+import 'package:path_provider/path_provider.dart';
 
 class WishRepository extends Equatable {
   const WishRepository({required WishApi wishApi}) : _wishApi = wishApi;
@@ -15,15 +18,47 @@ class WishRepository extends Equatable {
   }
 
   Future<void> saveWish(Wish wish) async {
+    Wish newWish = wish.copyWith(imagePath: await _moveImageFile(wish: wish));
+
     await _wishApi.saveWish(newWish);
   }
 
   Future<void> deleteWish(Wish wish) async {
+    if (wish.imagePath != null) {
+      _deleteImageFile(path: wish.imagePath!);
+    }
+
     await _wishApi.deleteWish(wish.id);
   }
 
   Future<void> addSaving(String id, Saving saving) async {
     await _wishApi.addSaving(id, saving);
+  }
+
+  Future<String?> _moveImageFile({required Wish wish}) async {
+    String? oldImagePath = wish.imagePath;
+
+    // move the cropped image file from cache to app directory
+    if (oldImagePath != null) {
+      Directory dir = await getApplicationDocumentsDirectory();
+
+      String newDirPath = '${dir.path}/images/';
+
+      Directory(newDirPath).createSync(recursive: true);
+
+      String newFileName = '${wish.name.replaceAll(' ', '-')}_${wish.createdAt.toUrl()}.jpg';
+
+      String newFilePath = '$newDirPath$newFileName';
+
+      File newFile = File(oldImagePath).renameSync(newFilePath);
+
+      return newFile.path;
+    }
+    return null;
+  }
+
+  void _deleteImageFile({required String path}) {
+    File(path).deleteSync();
   }
 
   @override
